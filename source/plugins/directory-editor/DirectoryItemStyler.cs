b00a92017bf156d73aa137d1f197c7cb1934dcb0
Jesse Jones <jesse9jones@gmail.com>
2012-07-04 05:29:48 -0700

    Made the nib/image directory item color a brighter green

--- source/plugins/directory-editor/DirectoryItemStyler.cs
+++ source/plugins/directory-editor/DirectoryItemStyler.cs
@@ -136,7 +136,7 @@ namespace DirectoryEditor
 			DoSetColor(dict, "path", null, 0, 0, 0);
 			DoSetColor(dict, "files1", "*.cs *.c *.cpp *.h *.hpp *.m *.rs *.js", 0, 0, 0);
 			DoSetColor(dict, "files2", "Makefile Make.shared *.am *.make *.mk SConstruct SConscript wscript wscript_build *.rc", 127, 21, 24);
-			DoSetColor(dict, "files3", "*.nib *.xib *.icns *.png *.jpeg *.jpg *.gif *.ignore", 18, 94, 16);
+			DoSetColor(dict, "files3", "*.nib *.xib *.icns *.png *.jpeg *.jpg *.gif *.ignore", 29, 151, 26);
 			DoSetColor(dict, "files4", "*.xml *.xsd *.xsdl *.xsl *.schema *.config *.plist *.sdef *.html", 83, 83, 151);
 			DoSetColor(dict, "files5", "*.css *.sh *.py *.rb *.pl *.ref", 61, 82, 194);
 			DoSetColor(dict, "files6", "*.dll *.exe *.dylib", 255, 0, 0);
